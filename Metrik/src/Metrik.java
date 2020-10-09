18bc6616cf36fb2fbee5735b05e9c6cd0c6025fc
u35613 <u35613@PC-Pool-VIL-27.vw.hs-harz.de>
2020-10-09 11:02:50 +0200

    Test Canvas

--- Metrik/src/Metrik.java
+++ Metrik/src/Metrik.java
@@ -1,9 +1,35 @@
 
+import java.awt.*;  
 public class Metrik {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		First2 f=new First2();  
 	}
+	
+}
+	class First2{  
+		final int width = 1200;
+	    final int height = width / 16 * 9;
+		First2(){  
+		Frame f=new Frame();  
+		Button b=new Button("click me");  
+		b.setBounds(30,50,80,30);  
+		f.add(b);  
+		f.setSize(300,300);  
+		f.setLayout(null);  
+		f.setVisible(true);  
+		//Creating the canvas.
+	    Canvas canvas = new Canvas();
+
+	    canvas.setSize(width, height);
+	    canvas.setBackground(Color.BLACK);
+	    canvas.setVisible(true);
+	    canvas.setFocusable(false);
+
+
+	    //Putting it all together.
+	    f.add(canvas);
 
 }
+		}
